9f43e7b59ee28e073fa661113a07b12226210178
Suat <suatisik@proton.me>
2026-05-10 16:06:15 +0300

    immigration

--- app/Views/front/home.php
+++ app/Views/front/home.php
@@ -7,7 +7,7 @@ $page_canonical   = $siteBase . '/';
 $og_url           = $page_canonical;
 $og_type          = 'website';
 $og_image         = $siteBase . '/cdn/notlar-social-preview.png';
-
+// deneme
 $extra_jsonld = [
     [
         '@context'  => 'https://schema.org',
