b9c9b32d812a4b000b2d88b05fac039582e775b9
Suat <suatisik@proton.me>
2026-05-10 17:12:09 +0300

    deneme 69

--- app/Views/front/home.php
+++ app/Views/front/home.php
@@ -341,7 +341,7 @@ require_once ROOT . '/app/Views/inc/header.php';
             <p class="text-xs mt-2 uppercase tracking-widest opacity-60 text-[var(--text-main)]">Toplam <?php echo isset($articles) ? count($articles) : 0; ?> Makale</p>
         </a>
     </div>
-    <!-- deploy deneme 33 -->
+    <!-- deploy deneme 67 -->
 </main>
 
 <?php 
